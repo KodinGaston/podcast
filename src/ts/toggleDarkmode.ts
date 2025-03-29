6032f865866da95471344f379dae7d60ea9c0259
kodingaston <gaston.sanchez@medieinstitutet.se>
2025-03-29 15:55:40 +0100

    change: btn-hover in mobiles

--- src/ts/toggleDarkmode.ts
+++ src/ts/toggleDarkmode.ts
@@ -4,10 +4,14 @@ toggleLightModeButton.addEventListener('click', toggleLightMode);
 
 export function toggleLightMode() {
   document.body.classList.toggle('darkmode');
+
   if (document.body.classList.contains('darkmode')) {
-    toggleLightModeButton.innerHTML = 'Byt till mörkt läge';      
+    toggleLightModeButton.innerHTML = 'Byt till mörkt läge';
+    toggleLightModeButton.style.backgroundColor = 'turquoise'; // Asegura que el color del botón se mantenga
   } else {
-    toggleLightModeButton.innerHTML = 'Byt till ljust läge';      
+    toggleLightModeButton.innerHTML = 'Byt till ljust läge';
+    toggleLightModeButton.style.backgroundColor = 'aqua'; // O el color que desees en modo claro
   }
 }
+
 export default toggleLightMode;
